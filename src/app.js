19b20a32e7166d5dac93b47cb7802a62f627bf95
Akmaljon571 <ahmadjonovakmal079@gmail.com>
2024-09-21 13:49:15 +0300

    meter va folder qoshishlar

--- src/app.js
+++ src/app.js
@@ -1,5 +1,8 @@
 const express = require('express');
 const cors = require('cors');
+const cookieParser = require('cookie-parser');
+const { router } = require('./router');
+const { ErrorHandle } = require('./middleware/error_handler.js');
 require('dotenv').config();
 
 const app = express();
@@ -8,5 +11,8 @@ process.env.TZ = 'Asia/Tashkent';
 
 app.use(cors());
 app.use(express.json());
+app.use(cookieParser());
+app.use(router);
+app.use(ErrorHandle);
 
 app.listen(PORT, console.log('Ishni boshladik'))
